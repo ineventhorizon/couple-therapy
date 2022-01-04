3ca37d02dc0c74b482ff97dd6f5dd3d1c4c00ef9
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2022-01-04 16:17:47 +0300

    + Refactore : just delete start and update function

--- Assets/Game/Scripts/RoadManager.cs
+++ Assets/Game/Scripts/RoadManager.cs
@@ -10,21 +10,12 @@ public class RoadManager : MonoBehaviour
     [SerializeField] private Transform parent;
     [SerializeField] private List<GameObject> roads;
     [SerializeField] private List<Transform> roadPath;
+
     [BoxGroup("Road Settings")]
     public float roadLength = 14f;
     [BoxGroup("Road Settings")]
     public int roadCount;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     [Button("Create")]
     private void CreateRoad()
@@ -36,6 +27,7 @@ public class RoadManager : MonoBehaviour
         roadPath.Add(newRoad.transform);
 
     }
+    
     [Button("Delete")]
     void DeleteRoad()
     {
