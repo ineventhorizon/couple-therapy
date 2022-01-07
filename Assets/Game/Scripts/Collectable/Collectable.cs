fae9a7247476433707e87c1d59dac95e2ec3d47a
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2022-01-07 19:21:42 +0300

    + Fix : fix everything..

--- Assets/Game/Scripts/Collectable/Collectable.cs
+++ Assets/Game/Scripts/Collectable/Collectable.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Collectable : MonoBehaviour
 {
     [SerializeField] private CollectableType type;
     [SerializeField] private float value;
+    Vector3 rot = new Vector3(0, 180, 0);
 
+    private void Start()
+    {
+        HandleAnimation();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -15,6 +21,12 @@ public class Collectable : MonoBehaviour
             var tmpValue = type == CollectableType.Positive ? value : -value;
             Observer.updateLove?.Invoke(tmpValue);
         }
+    }
+
+    private void HandleAnimation()
+    {
+        transform.DOLocalMoveY(2f, 1f, false).SetLoops(-1, LoopType.Yoyo);
+        transform.DORotate(rot, 2f, RotateMode.Fast).SetLoops(-1).SetEase(Ease.Linear);
     }
 }
 
